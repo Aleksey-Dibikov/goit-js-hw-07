58d87d109ef552b0d155dfc5227e3f573bf59fdc
Aleksey Dibikov <dibikovaleksey@gmail.com>
2021-07-13 23:11:29 +0300

    Updete task#2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -10,3 +10,13 @@ const ingredients = [
 // Напиши скрипт, который для каждого элемента создаст ingredients отдельный li,
 // после чего вставит все li за одну операцию в список ul.ingredients.
 // Для создания DOM - узлов используй document.createElement().
+
+const listElem = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const itemElem = document.createElement('li');
+  itemElem.textContent = ingredient;
+  return itemElem;
+});
+console.log(elements);
+listElem.append(...ingredients)
